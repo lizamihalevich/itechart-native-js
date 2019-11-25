d4e2b1461568e2636698bc7492622b39607b7dae
Lizaveta Mikhalevich <Lizaveta.Mikhalevich@itechart-group.com>
2019-11-25 13:53:28 +0300

    Fixed reduce method

--- libs/reduce.js
+++ libs/reduce.js
@@ -2,12 +2,13 @@ var reduce = function(arr, callback, initialVal) {
     if (!Array.isArray(arr)) {
         throw new Error("You should pass an array")
     }
-    var accumulator = (initialVal === undefined) ? undefined : initialVal;
+    var accumulator = initialVal;
     for (var i = 0; i < arr.length; i += 1) {
-        if (accumulator !== undefined)
-            accumulator = callback.call(undefined, accumulator, arr[i], i, arr);
-        else
+        if (!accumulator) {
             accumulator = arr[i];
+        } else {
+            accumulator = callback(accumulator, arr[i], i, arr);
+        }
     }
     return accumulator;
 };
